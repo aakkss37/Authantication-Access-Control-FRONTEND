86203725d00105371f1a64f5a64ef4ee73075bbe
amar kumar <amarkumar.sharma.124@gmail.com>
2023-07-09 23:16:08 +0530

    Registration page styled and form validatiion

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./Register";
+
+function App() {
+	return (
+		<main style={{ maxWidth: "800px", margin: "auto" }}>
+				<Register />
+		</main>
+	);
+}
+
+export default App;
